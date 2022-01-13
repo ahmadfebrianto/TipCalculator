289e66280589079e7f941931394d8605aa0b36da
Ahmad Febrianto <achmadfebryanto@gmail.com>
2022-01-13 10:16:34 +0700

    Add calculation logic

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.tipcalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.example.tipcalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvFinalCost.text = getString(R.string.final_cost, "0.00")
+        binding.edtCost.addTextChangedListener {
+            val value = binding.edtCost.text
+            binding.btCalculate.isEnabled = value.isNotEmpty()
+        }
+        binding.btCalculate.setOnClickListener { calculateTip() }
+
+    }
+
+
+    private fun calculateTip() {
+        val costOfService = binding.edtCost.text.toString().toDouble()
+        val tipPercentage = when (binding.rgServiceQuality.checkedRadioButtonId) {
+            binding.rbServiceAmazing.id -> 0.2
+            binding.rbServiceGood.id -> 0.18
+            else -> 0.15
+        }
+        var tip = costOfService * tipPercentage
+        if (binding.swRoundUpTip.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val finalCost = (costOfService + tip).round()
+        binding.tvFinalCost.text = getString(R.string.final_cost, finalCost.toString())
+    }
+
+    private fun Double.round(decimals: Int = 2): Double {
+        return "%.${decimals}f".format(this).toDouble()
     }
 }
